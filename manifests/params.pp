95fc028bec4e6f6ad52d8fff25e5fd4781c92065
mehmet.y <mehmetyazicioglu@gmail.com>
2017-04-13 10:37:12 +0000

    adjust params class

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,17 +1,27 @@
-class zabbixagent (
+class zabbixagent::params {
 
-$package_name   = $zabbixagent::params::package_name,
-$service_name   = $zabbixagent::params::service_name,
-$folder_log     = $zabbixagent::params::folder_log,
-$file_conf      = $zabbixagent::params::file_conf,
-$template       = $zabbixagent::params::template,
-$ensure         = $zabbixagent::params::ensure,
-$enable         = $zabbixagent::params::enable,
-$zserver        = $zabbixagent::params::zserver
+$ensure=running 
+$enable=true
+$zserver='10.4.4.97'
 
-)inherits zabbixagent::params
-{
 
-include zabbixagent::install
+    case $::osfamily {
+	    Debian: {
+		    $package_name 	= 'zabbix-agent'
+		    $service_name 	= 'zabbix-agent'
+		    $folder_log 	= '/var/log/zabbix'
+		    $file_conf 	    = '/etc/zabbix/zabbix_agentd.conf' 
+            $template       = 'zabbixagent/zabbix_agentd.conf.erb'
+	    }
+	    RedHat: {
+		    $package_name   = ''
+            $service_name   = ''
+            $folder_log     = ''
+            $file_conf      = ''
+	    }
+	    default: {
+		    fail("Modulo Zabbixagent nao suportado pelo sistema: ${::osfamily}")
+	    }
+    }
 
 }
